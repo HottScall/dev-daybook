8eb41502f8e18ff67bac32a5d2a6bc217b5fb2a1
Scott <Scotthall1983@hotmail.com>
2020-05-31 02:13:46 +0100

    initially printed the params and a string explaining the database details are being pulled through, got new failures but details from test were being pushed through. Then set 2 x variables of title and log equal to params. Crated a connection  to the database and insertted both the title and url using string  interpolation. Tests now pass. Refactor required.

--- app.rb
+++ app.rb
@@ -12,5 +12,13 @@ class DevDaybook < Sinatra::Base
     erb :'dailylogs/create'
   end
 
+  post '/dailylogs' do
+    title = params['title']
+    log = params['log']
+    connection = PG.connect(dbname: 'dev_daybook_test')
+    connection.exec("INSERT INTO dailylogs (title, log) VALUES('{#title}, {#log}')")
+    p "Form data submitted from /dailogs route"
+  end
+
   run! if app_file == $0
 end
